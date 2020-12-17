f45efc50a438149191d0236b14831c6ca9cf713c
mahmoudrezenaseri <=mahmoudreza.naseri@gmail.com>
2020-12-17 19:27:27 +0330

    clean up checkIfAdmin function

--- api/src/functions/common.js
+++ api/src/functions/common.js
@@ -26,10 +26,8 @@ async function checkIfAdmin(req, secretId) {
     const check = await User.CheckToken(req, secretId)
     if (check) {
         let user = await User.findById(check.id);
-        console.log(user)
         return (user.level === 1) ? true : false;
     }
-    console.log("user not found")
 
     return false;
 }
